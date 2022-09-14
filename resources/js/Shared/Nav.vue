894d43f3ba302bd8578a4b3e63e2eee16b53d57f
bellsilver7 <bellsilver7@gmail.com>
2022-09-15 00:01:15 +0900

    ✨ inertia Link

---
+++ resources/js/Shared/Nav.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+</script>
+
+<template>
+    <nav>
+        <ul class="flex">
+            <li class="text-sm bg-indigo-200 rounded px-2 py-1 mx-1">
+                <Link href="/">Home</Link>
+            </li>
+            <li class="text-sm bg-indigo-200 rounded px-2 py-1 mx-1">
+                <Link href="/users">Users</Link>
+            </li>
+            <li class="text-sm bg-indigo-200 rounded px-2 py-1 mx-1">
+                <Link href="/settings">Settings</Link>
+            </li>
+        </ul>
+    </nav>
+</template>

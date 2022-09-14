189006c6ec4abff1ac5baf88740db9613747aba5
bellsilver7 <bellsilver7@gmail.com>
2022-09-15 07:34:55 +0900

    Extract Layouts

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Nav from "@/Shared/Nav.vue";
 import { Link } from "@inertiajs/inertia-vue3";
+import Layout from "@/Shared/Layout.vue";
 
 defineProps({
     time: String,
@@ -8,11 +8,14 @@ defineProps({
 </script>
 
 <template>
-    <h1 class="text-4xl font-bold">Users</h1>
-    <Nav />
+    <Layout>
+        <h1 class="text-3xl">Users</h1>
 
-    <div style="margin-top: 400px">
-        <p>The current time is {{ time }}</p>
-        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
-    </div>
+        <div style="margin-top: 400px">
+            <p>The current time is {{ time }}</p>
+            <Link href="/users" class="text-blue-500" preserve-scroll
+                >Refresh</Link
+            >
+        </div>
+    </Layout>
 </template>

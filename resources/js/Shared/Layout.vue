41054efa62ccbce48055432e9a92deea19fb4ce5
bellsilver7 <bellsilver7@gmail.com>
2022-09-16 06:45:37 +0900

    Shared Data

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,11 +1,18 @@
 <script setup>
 import Nav from "@/Shared/Nav.vue";
+import { computed } from "vue";
+import { usePage } from "@inertiajs/inertia-vue3";
+
+const user = computed(() => usePage().props.value.auth.user);
 </script>
 
 <template>
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
-            <h1 class="font-bold text-lg">My app</h1>
+            <div class="flex items-center">
+                <h1 class="font-bold text-lg">My app</h1>
+                <p class="text-sm ml-4">Welcome back, {{ user.name }}!</p>
+            </div>
             <Nav />
         </header>
     </section>

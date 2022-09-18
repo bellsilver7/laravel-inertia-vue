5835f8cea991011d1f016d7eadab6ca7ad2c17d2
bellsilver7 <bellsilver7@gmail.com>
2022-09-18 21:09:04 +0900

    Filtering / State Preservation / Eloquent Search

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,15 +1,38 @@
 <script setup>
 import Pagination from "@/Shared/Pagination.vue";
+import { Inertia } from "@inertiajs/inertia";
+import { ref, watch } from "vue";
 
-defineProps({
+let props = defineProps({
     time: String,
     users: Array,
+    filters: Object,
+});
+
+let search = ref(props.filters.search);
+
+watch(search, (value) => {
+    Inertia.get(
+        "/users",
+        { search: value },
+        {
+            preserveState: true,
+            replace: true,
+        }
+    );
 });
 </script>
 
 <template>
-    <h1 class="text-3xl">Users</h1>
-
+    <div class="flex justify-between mb-6">
+        <h1 class="text-3xl">Users</h1>
+        <input
+            v-model="search"
+            type="text"
+            placeholder="Search..."
+            class="border px-2 rounded-lg"
+        />
+    </div>
     <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div
